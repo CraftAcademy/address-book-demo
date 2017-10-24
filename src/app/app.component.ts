47fd29b4c422473593166c9f67bdd3ec69551c57
Raoul DIFFOUO <diraulo@gmail.com>
2017-10-24 16:40:34 +0200

    create info card component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  contacts: any;
+
+  constructor(private http: Http, public addressBookService: AddressBookService) {
+    this.addressBookService.getContacts().subscribe(data => {
+      console.log(data);
+      this.contacts = data.contacts;
+    });
+  }
+
+  // getContacts() {
+  //   return this.http.get('http://ca-address-book.herokuapp.com/api/contacts')
+  //     .map((res: Response) => res.json());
+  // }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { InfoCardComponent } from './info-card/info-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InfoCardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
